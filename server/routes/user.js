6f9d50463278be0177d93f1dc6dee4bd19d3c792
Tonny Oseko <tonnyoseko0@gmail.com>
2023-02-01 22:47:41 +0300

    define routes for auth login and signup

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../models/user");
+const userController = require("../controllers/user");
 
+// user registration
+router.post("/register", userController.signup);
 
+// user login
+router.post("/login", userController.login);
+
+module.exports = router;
