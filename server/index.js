4a728d72cc9c827a87ac3e03d8d9107a3bbc3631
Tonny Oseko <tonnyoseko0@gmail.com>
2023-02-01 23:03:32 +0300

    import user routes to primary file

--- server/index.js
+++ server/index.js
@@ -2,8 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
+const userRoutes = require("./routes/user");
+
+// middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
 const { dbUrl } = require("./config/index");
 
 // MongoDB connection
@@ -16,6 +24,10 @@ app.get("/", (req, res) => {
   res.send("Successfully working!");
 });
 
+// route middlewares
+app.use("/auth", userRoutes);
+
+// listen to port
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
